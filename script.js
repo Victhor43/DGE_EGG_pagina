5dbd7e07d75ba1d764e67a6fb366271718e6341d
Victhor43 <asis.victor.92@gmail.com>
2023-05-24 15:26:11 -0300

    prueba

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
   return regex.test(email);
 }
+/* function validateForm() {
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value;
+  if (!validateEmail(email)) {
+    alert("Por favor ingrese un correo electrónico válido.");
+  } else {
+    alert("Correo electrónico enviado correctamente.");
+  }
+} */
 function validateForm() {
   const emailInput = document.getElementById("email");
   const email = emailInput.value;
